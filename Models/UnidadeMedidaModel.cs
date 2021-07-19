e404d2fcfcbfa8256b1c644e5a8e427387966d4d
SAMIACATD <samiacatd@gmail.com>
2021-07-19 19:22:51 -0300

    Criando models

---
+++ Models/UnidadeMedidaModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AlimentosTFOQ.Models
+{
+    public class UnidadeMedidaModel
+    {
+        public int Id { get; set; }
+
+        public string Nome { get; set; }
+
+        public bool Ativo { get; set; }
+    }
+}
